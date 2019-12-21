232a0cfbfa3db98498a4edf2cdb710129515266c
mateusfg7 <mateusfelipefg77@gmail.com>
2019-12-21 11:26:42 -0300

    add percentageon on analise_de_sentimento.py #3

--- functions/analise_de_sentimento.py
+++ functions/analise_de_sentimento.py
@@ -1,7 +1,15 @@
+import math
+
 from translate.traduzir import traduzir
 
 def analise_de_sentimento(client, sentenca):
     input = {"sentence": traduzir('en', sentenca)}
+    
     algo = client.algo('nlp/SocialSentimentAnalysis/0.1.4')
     retorno = algo.pipe(input).result
-    print("Positivo: {}\nNegativo: {}\nNeutro: {}".format(retorno[0]['positive'], retorno[0]['negative'], retorno[0]['neutral']))
+
+    positivo = math.floor(retorno[0]['positive'] * 100)
+    negativo = math.floor(retorno[0]['negative'] * 100)
+    neutro = math.floor(retorno[0]['neutral'] * 100)
+
+    print("Positivo: {}%\nNegativo: {}%\nNeutro: {}%".format(positivo, negativo, neutro))
