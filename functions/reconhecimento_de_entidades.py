35a0331aac660b61c24d323c0cf5e755f155cb69
mateusfg7 <mateusfelipefg77@gmail.com>
2019-12-23 10:25:38 -0300

    bug ao tentar analisar um arquivo de texto em branco #4

--- functions/reconhecimento_de_entidades.py
+++ functions/reconhecimento_de_entidades.py
@@ -1,17 +1,24 @@
 from translate.traduzir import traduzir
 
 def reconhecimento_de_entidades(client, arquivo):
-    texto = traduzir('en', arquivo)
-    input = {"document": texto}
-    algoritimo = client.algo('StanfordNLP/NamedEntityRecognition/0.2.0')
-    resultado = algoritimo.pipe(input).result
+    
+    if len(arquivo) != 0:
 
-    numeroDeEntidadesEncontradas = len(resultado['sentences'])
-    wordlist = resultado['sentences'][numeroDeEntidadesEncontradas - 1]['detectedEntities']
+        texto = traduzir('en', arquivo)
+        input = {"document": texto}
 
-    if len(wordlist) == 0:
-        print("Não foram encontradas nenhuma entidade.")
-        sys.exit(1)
+        algoritimo = client.algo('StanfordNLP/NamedEntityRecognition/0.2.0')
+        resultado = algoritimo.pipe(input).result
+
+        numeroDeEntidadesEncontradas = len(resultado['sentences'])
+        wordlist = resultado['sentences'][numeroDeEntidadesEncontradas - 1]['detectedEntities']
+
+        if len(wordlist) == 0:
+            print("Não foram encontradas nenhuma entidade.")
+            exit()
+        else:
+            for name in wordlist:
+                print('Nome: {} \nEntidade: {}\n'.format(name['word'], traduzir('pt', name['entity']).capitalize()))
+    
     else:
-        for name in wordlist:
-            print('Nome: {} \nEntidade: {}\n'.format(name['word'], traduzir('pt', name['entity']).capitalize()))
+        print("Texto em branco.")
