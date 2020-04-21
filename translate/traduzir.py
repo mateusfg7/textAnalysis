74b6a2dace8685f8fd25154b95fc6b9e65d73573
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 16:06:21 -0300

    use function in google trans

--- translate/traduzir.py
+++ translate/traduzir.py
@@ -1,12 +1,14 @@
 import sys
 
+from interface import texts
+
 try:
     from googletrans import Translator
     translator = Translator()
 except ModuleNotFoundError:
-    print("Biblioteca 'googletrans' não foi encontrada.")
-    print("tente: pip3 install googletrans")
+    print(texts.moduleNotFoundError('GoogleTrans', 'googletrans'))
     sys.exit(1)
+
 
 def traduzir(language, text):
     return translator.translate(text, dest=language).text
