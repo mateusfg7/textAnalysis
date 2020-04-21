5346e8eff426daa601aa587a6d1c9265bccc5e9d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-21 15:28:27 -0300

    add util/ folder

---
+++ utils/readFile.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def read_file(file):
+    try:
+        with open(file, 'r') as openFile:
+            return openFile.read()
+
+    except FileNotFoundError:
+
+        if file == 'analysis.py0':
+            print('Nenhum arquivo foi passado!')
+        else:
+            print('Arquivo "{}" não encontrado!'.format(file))
+        sys.exit()
