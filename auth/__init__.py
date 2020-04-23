7d727b1a49fd4a0693abed9f3017be8e222fe0f5
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-23 13:45:02 -0300

    create 'createKey' method

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,7 +1,14 @@
 def getKey(readFile, str2json):
-    strKeys = readFile('auth/keyss.json')
+    strKeys = readFile('auth/keys.json')
     if strKeys:
         jsonKeys = str2json(strKeys)
         return jsonKeys
     else:
         return False
+
+
+def createKey(key, writeFile):
+    jsonKeys = f'{"{"}\n "api_key": "{key}"\n{"}"}'
+    strKeys = f''
+    writeFile('auth/keys.json', jsonKeys)
+    return True
