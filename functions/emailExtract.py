587ace6864eb0338f499c73df5d664d91ffaf17f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-24 16:51:12 -0300

    update function 'emailExtract' display

--- functions/emailExtract.py
+++ functions/emailExtract.py
@@ -1,4 +1,7 @@
 def emailExtract(client, text):
     algo = client.algo('cindyxiaoxiaoli/EmailExtractor/0.2.0')
     response = algo.pipe(text).result
-    print(response)
+    if len(response) >= 0:
+        print(f'foram encontrados {len(response)} emails:\n')
+        for item in response:
+            print(f'{item}')
