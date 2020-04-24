f474f9f4c7a67dfdd90fb5a33036de1b4a99df1f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-24 17:13:51 -0300

    clean code: add return type in 'checkInternetConnection' and use 'sys.exit()' in 'internetFailWarning'

--- utils/connection.py
+++ utils/connection.py
@@ -1,13 +1,16 @@
+import sys
 import requests
 
-def checkInternetConnection():
+
+def checkInternetConnection() -> bool:
     try:
         requests.get('https://algorithmia.com/')
         return True
     except:
         return False
 
+
 def internetFailWarning():
     print("Você não tem conexão com a internet.")
     print("Esse algorítimo precisa de conexão com a internet para ser executado.")
-    exit()
+    sys.exit()
