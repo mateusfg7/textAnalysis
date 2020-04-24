915fe5428db8a0c4306adcb2cab68e94abb21b34
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-24 17:06:25 -0300

    use title text in 'emailExtract'

--- functions/emailExtract.py
+++ functions/emailExtract.py
@@ -1,4 +1,8 @@
+from interface.texts import optionsTitle
+
+
 def emailExtract(client, text):
+    print(optionsTitle('--email'))
     algo = client.algo('cindyxiaoxiaoli/EmailExtractor/0.2.0')
     response = algo.pipe(text).result
     if len(response) >= 0:
