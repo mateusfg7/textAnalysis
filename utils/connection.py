6fa872515db9fd86e8a6b52459d2c2079423011a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-04-24 17:37:27 -0300

    add types in 'checkInternetConnection'

--- utils/connection.py
+++ utils/connection.py
@@ -1,9 +1,6 @@
-import requests
-
-
-def checkInternetConnection() -> bool:
+def checkInternetConnection(request: "function") -> bool:
     try:
-        requests.get('https://algorithmia.com/')
+        request('https://algorithmia.com/')
         return True
     except:
         return False
