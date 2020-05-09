06d7ed4b62a18655272f9da2556d1fb30e43a946
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-09 10:38:10 -0300

    dependencies verification style

--- AnalysisGUI.py
+++ AnalysisGUI.py
@@ -17,11 +17,58 @@ from utils.fileTreatment import readFile
 from utils.fileTreatment import writeFile
 
 
+cores = {
+    'reset': '\033[0;0;0m',
+    'red': '\033[1;31m',
+    'liteRed': '\033[1;91m',
+    'green': '\033[1;92m',
+    'liteGreen': '\033[1;92m',
+    'black': '\033[1;30m',
+    'yellow': '\033[1;33m',
+    'liteYellow': '\033[1;93m',
+    'blue': '\033[1;34m',
+    'liteBlue': '\033[1;94m',
+    'magenta': '\033[1;35m',
+    'magentaClaro': '\033[1;95m',
+    'cyan': '\033[1;36m',
+    'cyanClaro': '\033[1; 96m',
+    'liteGray': '\033[1;37m',
+    'darkGray': '\033[1;90m',
+    'white': '\033[1;97m',
+    'bold': '\033[;1m',
+    'invert': '\033[;7m',
+}
+
+print(f'Verificando dependencias...\n{cores["reset"]}')
+
 try:
     import Algorithmia
+    print(
+        f'Algorithmia {cores["green"]}OK{cores["reset"]}'
+    )
 except ModuleNotFoundError:
-    print(texts.moduleNotFoundError('Algorithmia', 'algorithmia'))
+    print(
+        f'Algorithmia {cores["red"]}NOT FOUND{cores["reset"]}'
+    )
+    print(
+        f'install: https://algorithmia.com/developers/clients/python{cores["reset"]}'
+    )
     sys.exit(1)
+
+try:
+    import googletrans
+    print(
+        f'GoogleTrans {cores["green"]}OK{cores["reset"]}'
+    )
+except ModuleNotFoundError:
+    print(
+        f'GoogleTrans {cores["red"]}NOT FOUND{cores["reset"]}'
+    )
+    print(
+        f'install: https://pypi.org/project/googletrans/{cores["reset"]}'
+    )
+    sys.exit(1)
+
 keys = getKey(readFile, str2json)
 if keys:
     CLIENT = Algorithmia.client(keys['api_key'])
