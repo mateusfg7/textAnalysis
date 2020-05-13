49b8eb21566dac0762410b3fbd8d05040a22a52f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-13 14:37:06 -0300

    fixing bugs in summarizeText.py

--- functions/summarizeText.py
+++ functions/summarizeText.py
@@ -1,11 +1,10 @@
-from utils.translate import traduzir
-from interface.texts import optionsTitle
+def summarizeText(client, text: str) -> 'NoReturn':
+    from utils.translate import traduzir
+    from interface import texts
 
+    print(texts.optionsTitle('summarize'))
 
-def summarizeText(client, texto: str) -> None:
-    print(optionsTitle('--summarize'))
-
-    translatedText: str = traduzir('en', texto)
+    translatedText: str = traduzir('en', text)
     algoritimo = client.algo('nlp/Summarizer/0.1.8')
     response: str = algoritimo.pipe(translatedText).result
 
