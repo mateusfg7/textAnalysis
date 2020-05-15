e4e6d621ad5bb53973ea430880281e182f7eda3f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-15 11:08:24 -0300

    fixing alert error bug in translate.py

--- utils/translate.py
+++ utils/translate.py
@@ -1,12 +1,13 @@
 import sys
 
+from utils.colors import style
 from interface import texts
 
 try:
     from googletrans import Translator
     translator = Translator()
 except ModuleNotFoundError:
-    print(texts.moduleNotFoundError('GoogleTrans', 'googletrans'))
+    print(texts.modules(style, 'googletrans', 'error'))
     sys.exit(1)
 
 
