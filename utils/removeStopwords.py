35540f59658e8807cbc2c833e53b28a3979f6d6b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-15 11:03:15 -0300

    typing removeStopwords

--- utils/removeStopwords.py
+++ utils/removeStopwords.py
@@ -1,4 +1,7 @@
-def removeStopwords(text, stopwords):
+from typing import List
+
+
+def removeStopwords(text: str, stopwords: List[str]) -> str:
     for stopword in stopwords:
         text = text.replace(stopword, ' ')
     return text
