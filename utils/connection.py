afeba8ec2a867c734c1e5d93c5aeac83c5ce6a00
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-27 11:02:16 -0300

    adding description in the 'checkInternetDescription' function

--- utils/connection.py
+++ utils/connection.py
@@ -2,6 +2,14 @@ from typing import Callable, NoReturn
 
 
 def checkInternetConnection(request: Callable[[str], NoReturn]) -> bool:
+    '''
+    This function needs function param request\n
+    example:\n
+    import request\n
+    checkInternetConnection(request.get)\n
+    \n
+    return True, if internet connection be ok, or False, if internet connection not be ok.
+    '''
     try:
         request('https://algorithmia.com/')
         return True
