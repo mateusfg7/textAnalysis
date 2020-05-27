8bd95da5b5cdcca3c90d81614598a51076e3d46c
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-27 10:11:43 -0300

    using __name__ == '__main__'

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,6 @@
+# types
+from typing import Callable, Union, NoReturn, Dict
+
 # GLOBAL MODULES
 import sys
 from json import loads as str2json
@@ -14,7 +17,6 @@ from check.checkModules import checkModules
 from check.clientKey import getClientKey
 from check.textToAnalyse import getTextToAnalyse
 
-from kernel import kernel_start
 
 # <start> rules for test
 try:
@@ -33,11 +35,43 @@ if checkModules(texts, style):
     import Algorithmia
     import googletrans
 
-kernel_start(
-    getClientKey,
-    getTextToAnalyse,
-    texts,
-    choices,
-    Algorithmia,
-    style
-)
+
+def start(getClientKey: Callable[['Module'], Union['AlgorithmiaClient', NoReturn]],
+          getTextToAnalyse: Callable[[], Union[str, NoReturn]],
+          texts: Callable[[Dict[str, str]], NoReturn],
+          choices: Callable[[str, 'Client', str], NoReturn],
+          Algorithmia: 'Module',
+          style: Dict[str, str]) -> NoReturn:
+    """
+        The kernel of algorithm
+        """
+    CLIENT = getClientKey(Algorithmia)
+    TEXT = getTextToAnalyse()
+
+    texts.clearAndShowHeader(style)
+    print(texts.choicesMenu(style))
+    choice = input('> ')
+
+    texts.clearAndShowHeader(style)
+    selectChoice = {
+        '1': lambda: choices('tag', CLIENT, TEXT),
+        '2': lambda: choices('feeling', CLIENT, TEXT),
+        '3': lambda: choices('summarize', CLIENT, TEXT),
+        '4': lambda: choices('entity', CLIENT, TEXT),
+        '5': lambda: choices('frequency', CLIENT, TEXT),
+        '6': lambda: choices('count', CLIENT, TEXT),
+        '7': lambda: choices('email', CLIENT, TEXT),
+        '8': lambda: choices('date', CLIENT, TEXT)
+    }
+    selectChoice[choice]()
+
+
+if __name__ == "__main__":
+    start(
+        getClientKey,
+        getTextToAnalyse,
+        texts,
+        choices,
+        Algorithmia,
+        style
+    )
