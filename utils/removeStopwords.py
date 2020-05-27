a68f01dfc85ce9ce21bbea08b5663cf62fe5a4e9
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-27 11:09:17 -0300

    adding description in the 'removeStopwords' function

--- utils/removeStopwords.py
+++ utils/removeStopwords.py
@@ -2,6 +2,9 @@ from typing import List
 
 
 def removeStopwords(text: str, stopwords: List[str]) -> str:
+    '''
+    Removes the chosen stopwords in the text then return the text without stopwords.
+    '''
     for stopword in stopwords:
         text = text.replace(stopword, ' ')
     return text
