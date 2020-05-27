e125075e222bed3dfa8284d86a224e3f63ec705d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-05-27 10:54:15 -0300

    adding api_key validation

--- check/clientKey.py
+++ check/clientKey.py
@@ -15,12 +15,24 @@ def getClientKey(Algorithmia: 'Module') -> Union['AlgorithmiaClient', NoReturn]:
         return Algorithmia.client(keys['api_key'])
     else:
         texts.clearAndShowHeader(style)
-        print('Visite https://github.com/mateusfg7/textAnalysis/blob/master/README.md para saber como criar sua api key.')
-        key = input('Sua api key: ')
-        if createKey(key, writeFile):
-            print('Api key salva com sucesso!')
-            keys = getKey(readFile, str2json)
-            return Algorithmia.client(keys['api_key'])
-        else:
-            print('Um erro ocorreu durante o processo, tente novamente.')
-            sys.exit()
+
+        def validadeKey():
+            print('Visite https://github.com/mateusfg7/textAnalysis/blob/master/README.md para saber como criar sua api key.')
+            key = input('Sua api key: ')
+            try:
+                # test request
+                Algorithmia.client(key).algo(
+                    'nlp/SocialSentimentAnalysis/0.1.4').pipe('example_text').result
+                if createKey(key, writeFile):
+                    print('Api key salva com sucesso!')
+                    keys = getKey(readFile, str2json)
+                    return Algorithmia.client(keys['api_key'])
+                else:
+                    print('Um erro ocorreu durante o processo, tente novamente.')
+                    sys.exit()
+            except Algorithmia.errors.AlgorithmException:
+                texts.clearAndShowHeader(style)
+                print('Api key inválida! adicione um api key válida.')
+                return validadeKey()
+        client = validadeKey()
+        return client
